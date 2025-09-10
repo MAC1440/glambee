b87eb7bd806f20435eee8392f4efec8cb2d1155f
Ahmad Afzal <ahmad.afzal@invozone.dev>
2025-09-10 10:54:05 +0000

    still not fixes

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -7,9 +7,7 @@ import { Card } from "@/components/ui/card";
 export function Dashboard() {
   return (
     <div className="container mx-auto p-4 flex flex-col gap-6">
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 w-full">
-        <StatCards />
-      </div>
+      <StatCards />
       <div className="grid gap-4 md:gap-8 lg:grid-cols-2">
         <AppointmentsTable />
         <Card>
