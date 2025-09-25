6285141b0cc7049619ff30f0b5dbe1a0dc87a465
Ahmad Afzal <ahmad.afzal@invozone.dev>
2025-09-25 10:36:53 +0000

    chart is not taking up all the space, we have a lot of empty space below

--- src/features/dashboard/RevenueChart.tsx
+++ src/features/dashboard/RevenueChart.tsx
@@ -14,7 +14,7 @@ const data = [
 
 export function RevenueChart() {
   return (
-    <ResponsiveContainer width="100%" height={300}>
+    <ResponsiveContainer width="100%" height="100%" minHeight={300}>
       <BarChart data={data}>
         <XAxis
           dataKey="month"
